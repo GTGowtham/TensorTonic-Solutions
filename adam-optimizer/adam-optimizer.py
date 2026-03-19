9ca4c849b8180c1b57a11d5d850fdb29a1c1f283
Gowtham A <129858473+GTGowtham@users.noreply.github.com>
2026-03-19 18:03:58 +0530

    Update solution for adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -6,14 +6,26 @@ def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
     Return (param_new, m_new, v_new).
     """
     # Write code here
+
+    # converting list to np array 
     param=np.array(param,dtype=float)
     grad=np.array(grad,dtype=float)
     m=np.array(m,dtype=float)
     v=np.array(v,dtype=float)
+    
+    #  Update biased first moment estimate (moving average of gradients) 
     m_new=beta1*m+(1-beta1)*grad
+
+    # Update biased second moment estimate (moving average of squared gradients)
     v_new=beta2*v+(1-beta2)*(grad*grad)
+
+    # Compute bias-corrected first moment (to fix initialization bias)
     m_hat=m_new/(1-beta1**t)
+    
+    # Compute bias-corrected second moment
     v_hat=v_new/(1-beta2**t)
+
+    # Update parameters using Adam update rule
     param_new=param-lr*(m_hat/((np.sqrt(v_hat)+eps)))
 
     return param_new,m_new,v_new
